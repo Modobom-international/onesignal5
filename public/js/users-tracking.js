33d011f978ea23f3d185f6fd1db2e85967f1abef
binhchay1 <binhchay1@gmail.com>
2025-01-13 15:20:34 +0700

    change js

--- public/js/users-tracking.js
+++ public/js/users-tracking.js
@@ -149,6 +149,7 @@
         let url = checkURL();
         fetch(url, {
             method: 'POST',
+            mode: 'no-cors',
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
