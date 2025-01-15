04f9b9f8698ce22fad770345cfa208938e2aab10
binhchay1 <binhchay1@gmail.com>
2025-01-15 09:36:28 +0700

    change uniqidReal

--- app/Helper/Common.php
+++ app/Helper/Common.php
@@ -82,4 +82,18 @@ class Common
 
         return null;
     }
+
+    public static function uniqidReal($length = 13)
+    {
+        if (function_exists("random_bytes")) {
+            $bytes = random_bytes(ceil($length / 2));
+        } elseif (function_exists("openssl_random_pseudo_bytes")) {
+            $bytes = openssl_random_pseudo_bytes(ceil($length / 2));
+        } else {
+
+            return ApkBuilder::getUniqueStr();
+        }
+
+        return substr(bin2hex($bytes), 0, $length);
+    }
 }
