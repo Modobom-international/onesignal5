13537209664b76878ff7359b2aecebe23ed7b612
binhchay1 <binhchay1@gmail.com>
2025-02-17 06:41:00 +0000

    change app

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name='robots' content='noindex,nofollow' />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <title>{{ config('app.name', 'Modobom') }}</title>
 
