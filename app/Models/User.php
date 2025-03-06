5f8a1af91956c8e34b3c903d27f6ac9ec39887ba
L3oN <89580650+L3oN007@users.noreply.github.com>
2025-03-06 12:01:08 +0700

    feat(permissions): integrate Spatie Laravel Permission package

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Models\Team;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     const ADMIN_TYPE = 'admin';
     const USER_TYPE = 'user';
