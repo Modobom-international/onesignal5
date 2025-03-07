eb59282bd4b4039fe0b6bfcc60b21676c50a02db
binhchay1 <binhchay1@gmail.com>
2025-03-07 10:34:48 +0000

    change schedule

--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,9 @@ use App\Console\Commands\LogBehavior\NotifyTelegramInstallLogBehavior;
 use App\Console\Commands\LogBehavior\TransDataForOldYear;
 use App\Console\Commands\FillUrlForPushSystem;
 use App\Console\Commands\Domains\CheckDomain;
+use App\Console\Commands\LogBehavior\SyncOldData;
+use Illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Facades\Artisan;
 
 Schedule::command('horizon:snapshot')->everyFiveMinutes();
 Schedule::command('telescope:prune')->daily();
@@ -13,3 +16,14 @@ Schedule::command(NotifyTelegramInstallLogBehavior::class)->everyMinute();
 Schedule::command(CheckDomain::class)->everyMinute();
 Schedule::command(FillUrlForPushSystem::class)->dailyAt('00:00');
 Schedule::command(TransDataForOldYear::class)->yearly();
+
+Schedule::command(SyncOldData::class)
+    ->dailyAt('00:00')
+    ->then(function () {
+        $exitCode = Artisan::call('log-behavior:sync-old-data', [], new \Symfony\Component\Console\Output\NullOutput());
+        if ($exitCode === 0) {
+            Artisan::call('log-behavior:cache-data-per-date');
+        } else {
+            \Log::error('log-behavior:sync-old-data run failed, log-behavior:cache-data-per-date');
+        }
+    });
