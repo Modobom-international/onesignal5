0ccfcfb0bd40b2b47a01ac97acb23d7e876d8b11
binhchay1 <binhchay1@gmail.com>
2025-03-11 04:24:10 +0000

    turn off fech full

--- app/Http/Controllers/UsersTrackingController.php
+++ app/Http/Controllers/UsersTrackingController.php
@@ -65,7 +65,7 @@ class UsersTrackingController extends Controller
             ];
 
             StoreHeatMap::dispatch($dataHeatMap)->onQueue('create_heat_map');
-            FetchFullPage::dispatch($dataFetch)->onQueue('fetch_full_page');
+            // FetchFullPage::dispatch($dataFetch)->onQueue('fetch_full_page');
         }
 
         return response()->json(['message' => 'User behavior recorded successfully.']);
