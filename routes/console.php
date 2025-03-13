a182766d6b6fb61986130725405756d1487ebd98
binhchay1 <binhchay1@gmail.com>
2025-03-13 02:32:12 +0000

    change erorr

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Console\Commands\LogBehavior\NotifyTelegramInstallLogBehavior;
 use App\Console\Commands\LogBehavior\TransDataForOldYear;
 use App\Console\Commands\FillUrlForPushSystem;
 use App\Console\Commands\Domains\CheckDomain;
@@ -12,7 +11,6 @@ Schedule::command('horizon:snapshot')->everyFiveMinutes();
 Schedule::command('telescope:prune')->daily();
 Schedule::command('horizon:clear-log')->monthlyOn(1, '00:00');
 
-Schedule::command(NotifyTelegramInstallLogBehavior::class)->everyMinute();
 Schedule::command(CheckDomain::class)->everyMinute();
 Schedule::command(FillUrlForPushSystem::class)->dailyAt('00:00');
 Schedule::command(TransDataForOldYear::class)->yearly();
