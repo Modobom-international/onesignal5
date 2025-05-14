a46153253ef3f6dbe7511f4b5ae56c3158737721
binhchay1 <binhchay1@gmail.com>
2025-05-14 04:55:44 +0000

    fix config

--- config/database.php
+++ config/database.php
@@ -48,7 +48,10 @@ return [
             'port'     => env('DB_PORT_MONGO', 27017),
             'database' => env('DB_DATABASE_MONGO', 'onesignal5'),
             'username' => env('DB_USERNAME_MONGO', 'root'),
-            'password' => env('DB_PASSWORD_MONGO', '')
+            'password' => env('DB_PASSWORD_MONGO', ''),
+            'options'  => [
+                'authSource' => env('DB_AUTHENTICATION_DATABASE', 'admin'),
+            ],
         ],
 
         'mysql' => [
