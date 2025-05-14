dbe7e876e7818a81ce6afa8fb689faf02ff4d290
binhchay1 <binhchay1@gmail.com>
2025-05-14 04:19:14 +0000

    remove telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
     MongoDB\Laravel\MongoDBServiceProvider::class,
     App\Providers\BroadcastServiceProvider::class,
     Barryvdh\Debugbar\ServiceProvider::class,
